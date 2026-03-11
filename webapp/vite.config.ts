fcbaff28810e4c92235447ab50ca0237124a7c4b
UO301901 <uo301901@uniovi.es>
2026-03-12 00:25:06 +0100

    segundo intento de ejecucion de pruebas en deploy

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,6 +5,11 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: ["./vitest-setup.js"],
+    include: ["src/__tests__/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+    exclude: [
+      "**/node_modules/**",
+      "**/test/e2e/**"
+    ],
     coverage: {
       reporter: ["text", "lcov"],
     },

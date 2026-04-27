98e12013f319827e56759cf211867923c864074c
Pablo <uo301693@uniovi.es>
2026-04-28 01:49:16 +0200

    Arreglos security Hotspot

--- users/users-service.js
+++ users/users-service.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.disable('x-powered-by'); //lo pide sonar
 const port = 3000;
 const swaggerUi = require('swagger-ui-express');
 const fs = require('node:fs');

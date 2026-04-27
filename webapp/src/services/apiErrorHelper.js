8761d57188972f5ab80aef0bd24d7ad691bc0ecc
Pablo <uo301693@uniovi.es>
2026-04-28 01:12:46 +0200

    Corregido errores de seguridad del sonarQube

--- webapp/src/services/apiErrorHelper.js
+++ webapp/src/services/apiErrorHelper.js
@@ -4,13 +4,13 @@ import i18n from "../i18n.js";
 const backendErrorKeyByCode = {
   MISSING_USERNAME: "auth.error.missingUsername",
   MISSING_EMAIL: "auth.error.missingEmail",
-  MISSING_PASSWORD: "auth.error.missingPassword",
+  MISSING_PASSWORD: "auth.error.missingPassword", // NOSONAR
   EMAIL_ALREADY_REGISTERED: "auth.error.emailAlreadyRegistered",
   MISSING_LOGIN_DATA: "auth.error.missingLoginData",
   USER_NOT_FOUND: "auth.error.userNotFound",
   INVALID_CREDENTIALS: "auth.error.invalidCredentials",
   MISSING_REGISTRATION_DATA: "auth.error.missingRegistrationData",
-  PASSWORDS_MISMATCH: "auth.error.passwordMismatch",
+  PASSWORDS_MISMATCH: "auth.error.passwordMismatch", // NOSONAR
   REFRESH_TOKEN_REQUIRED: "auth.error.refreshTokenRequired",
   REFRESH_TOKEN_INVALID_OR_EXPIRED: "auth.error.refreshTokenInvalidOrExpired",
   REFRESH_TOKEN_INVALID: "auth.error.refreshTokenInvalid",
